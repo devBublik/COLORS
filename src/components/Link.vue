6cce1142ff7dffd470d3c85f35aa4d80fd4c8bba
e.bublikova <e.bublikova@cinet.ru>
2023-04-03 01:25:08 +0300

    init project: add header

---
+++ src/components/Link.vue
@@ -0,0 +1,21 @@
+<template>
+    <n-link :to='to' class="ui-link">
+        {{ label }}
+    </n-link>
+</template>
+
+<script>
+    export default {
+    name: 'UILink',
+    props: {
+        to: {
+            type: [String, Object],
+            default: () => '',
+        },
+        label: {
+            type: String,
+            default: () => ''
+        }
+    }
+}
+</script>
